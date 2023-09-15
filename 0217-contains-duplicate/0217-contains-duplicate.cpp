9377dacc8841aa797af0afcb94db325fa25e3964
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-15 15:16:41 +0530

    Time: 161 ms (8.96%), Space: 73.9 MB (8.66%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         /*
-            using sort and then check adjacent number
+            using set and comparing set to orignal
         */
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        
-        for(int i=0; i<n-1; i++)
-            if(nums[i] == nums[i+1])
-                return true;
+        set<int> myset;
 
-        return false;
+        for(auto i : nums)
+            myset.insert(i);
+
+        return myset.size()!=nums.size();
     }
 };
