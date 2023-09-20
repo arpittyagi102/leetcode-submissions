2e3f8f738e4274f67cdc71c8a6d41a58cc6ebd13
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-20 19:06:25 +0530

    Time: 127 ms (10.19%), Space: 8.9 MB (48.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,15 +4,17 @@ public:
         int n = s.size();
         int left = 0;
         int right = 0;
-        unordered_set<char> mp;
+        unordered_map<char,int> mp;
         int max = -1;
 
         for(right=0; right<n; right++){
-            while(mp.contains(s[right])){
-                mp.erase(s[left]);
-                left++;
+            cout<<"left = "<<left<<" right = "<<right<<endl;
+            if(mp.contains(s[right]) && mp[s[right]]>=left){
+                left = mp[s[right]] +1;
+                cout<<"map contains "<<s[right]<<", so left = "<<left<<endl;
             }
-            mp.insert(s[right]);
+            cout<<"adding "<<s[right]<<" to the map\ndifference is "<<right - left+1<<endl;
+            mp[s[right]] = right;
             if(max < right-left)
                 max = right-left;
         }
