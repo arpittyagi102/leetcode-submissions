f34c7e1bf4133770b00b2cd033cb85454503545c
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-24 16:49:07 +0530

    Time: 82 ms (58.37%), Space: 68.9 MB (20.81%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         vector<int> prefixgreat(arr.size());
-        int maxnum = -1;
-
-        for(int i=arr.size()-1; i>=0; i--){
-            prefixgreat[i] = maxnum;
-            maxnum = max(maxnum,arr[i]);
+        int maxhere = -1;
+        for(int i=arr.size()-1;i>=0;i--)
+        {
+            prefixgreat[i] = maxhere;
+            maxhere = max(maxhere,arr[i]);
         }
-
         return prefixgreat;
     }
 };
