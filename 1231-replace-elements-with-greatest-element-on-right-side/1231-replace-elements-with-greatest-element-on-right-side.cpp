37719381ca590b1e08a214fcdcfb0f1ec40b598a
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-24 16:31:56 +0530

    Time: 85 ms (45.86%), Space: 69 MB (18.23%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int n = arr.size();
-        int max = arr[n-1];
-        vector<int> res;
+        int maxnum = arr[n-1];
 
         for(int i=n-1; i>0; i--){
-            if(arr[i] > max)
-                max = arr[i];
-            cout<<max<<" ";
-            res.push_back(max);
+            maxnum = max(arr[i],maxnum);
+            arr[i] = maxnum;
         }
 
-        reverse(res.begin(),res.end());
-        res.push_back(-1);
+        arr.erase(arr.begin());
+        arr.push_back(-1);
 
-        return res;
+        return arr;
     }
 };
