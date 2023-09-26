8419b188a5bdae9dd25fc7a7744b41d2009c5284
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-26 10:50:43 +0530

    Time: 3 ms (87.55%), Space: 9.1 MB (16.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,30 +2,25 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> arr;
+        unordered_map<int, int> mp;
         int ek = -1;
 
-        for(int i=0; i<nums1.size(); i++){
-            cout<<"Finding "<<nums1[i]<<"..."<<endl;
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i]==nums2[j]){
-                    cout<<"Found at position : "<<j<<endl;
-                    for(int k=j+1; k<nums2.size(); k++){
-                        if(nums2[k] > nums2[j]){
-                            ek = nums2[k];
-                            cout<<"Next greater element is "<<nums2[k]<<endl;
-                            break;
-                        }
-                    }
-                    arr.push_back(ek);
-                    ek = -1;
-                    cout<<endl;
-                    continue;
+        for(int i=0; i<nums2.size(); i++){
+            for(int j=i+1; j<nums2.size(); j++){
+                if(nums2[i] < nums2[j]){
+                    ek = nums2[j];
+                    break;
                 }
             }
+            mp[nums2[i]] = ek;
+            ek = -1;
         }
-        
 
-        return arr;
+        for(int i=0; i<nums1.size(); i++){
+            nums1[i] = mp[nums1[i]];
+        }  
+
+        return nums1;
     }
 
 };
