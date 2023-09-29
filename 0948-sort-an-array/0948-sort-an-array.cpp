5b4d64db923e631a08992ed29bca468466c878de
Arpit Tyagi <arpittyagi102@gmail.com>
2023-09-29 10:35:44 +0530

    Time: 97 ms (89.69%), Space: 67.1 MB (67.91%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        if(nums.size()==1){
-            cout<<nums[0]<<" ";
-            return nums;
-        }
-
-        int n = nums.size()/2;
-        vector<int> first{nums.begin(),nums.begin()+n};
-        vector<int> second{nums.begin()+n,nums.end()};
-
-        first = sortArray(first);
-        second = sortArray(second);
-
-        vector<int> hello(first.size()+second.size());
-        merge(first.begin(),first.end(),second.begin(),second.end(),hello.begin());
-
-        return hello;
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
