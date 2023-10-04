5efcd873e471b88312e0022a4835b321d2994a07
Arpit Tyagi <arpittyagi102@gmail.com>
2023-10-04 16:56:13 +0530

    Time: 4 ms (33.08%), Space: 8.7 MB (10.71%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        unordered_map<int,int> mp;
+
+        for(int i : nums){
+            mp[i]++;
+        }
+
+        nums.clear();
+
+        for(int j=0; j<3; j++){
+            for(int i=0; i<mp[j]; i++){
+                nums.push_back(j);
+            }
+        }
     }
 };
