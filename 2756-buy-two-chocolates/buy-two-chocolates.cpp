3a6c22e19ad37c252bffb372644ed40f373a31aa
Arpit Tyagi <arpittyagi102@gmail.com>
2023-12-20 09:40:50 +0530

    Time: 23 ms (5.34%) | Memory: 47 MB (58.30%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int left = money - prices[0] - prices[1];
+
+        if(left >= 0){
+            return left;
+        }
+        else 
+            return money;
+    }
+};
