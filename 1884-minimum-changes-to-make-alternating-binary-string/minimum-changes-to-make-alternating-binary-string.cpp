dc3cbb44a2c98e65113262628140f8adb8932f95
Arpit Tyagi <arpittyagi102@gmail.com>
2023-12-24 21:45:52 +0530

    Time: 7 ms (8.31%) | Memory: 7.3 MB (86.65%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int count = 0;
+
+        for(int i=0; i<s.size(); i++){
+            if(i%2==0 && s[i]=='0'){
+                count++;
+            } 
+            else if(i%2==1 && s[i]=='1') {
+                count++;
+            }
+            else{
+                cout<<":F\n";
+            }
+        }
+
+        return count < (s.size()-count) ? count : (s.size()-count); 
+    }
+};
