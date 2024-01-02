6b5289460b7d01dde4a62ad036adc869ae0ed799
Arpit Tyagi <arpittyagi102@gmail.com>
2024-01-02 10:24:13 +0530

    Time: 19 ms (18.59%) | Memory: 31.1 MB (7.53%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) {
+        unordered_set<int> st;
+        vector< vector<int>> ans;
+        while(nums.size()){
+            vector<int> temp;
+            for(int i: nums){
+                if(!st.insert(i).second)
+                    temp.push_back(i);
+                else{
+                    cout<<i<<" ";
+                }
+            }
+
+            ans.push_back(vector<int>(st.begin(),st.end()));
+            nums = temp;
+            st.clear();
+            cout<<"\n size = "<<nums.size()<<endl;
+        }
+
+        return ans;
+    }
+};
