c309a78c22f6a39b51c13611d6a71fb001747002
Arpit Tyagi <arpittyagi102@gmail.com>
2024-01-03 10:59:36 +0530

    Time: 82 ms (6.98%) | Memory: 23.7 MB (12.29%) - LeetSync

--- 554-brick-wall/brick-wall.cpp
+++ 554-brick-wall/brick-wall.cpp
@@ -1,41 +1,21 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        int ans = 0;
         unordered_map<int,int> mp;
-        vector<vector<int>> pre(wall.size());
-        for(int i=0; i<wall.size(); i++){
-            pre[i].push_back(wall[i][0]);
-            for(int j=1; j<wall[i].size(); j++){
-                pre[i].push_back(pre[i][j-1]+wall[i][j]);
-            }
-        }
+        int ans = 0;
 
-        for(vector<int> t: pre){
-            for(int i=0; i<t.size()-1; i++){
-                mp[t[i]]++;
-            }
+        for(vector<int> row: wall){
+            int n = 0;
+            for(int i=0; i<row.size()-1; i++){
+                n += row[i];
+                mp[n]++;
+            }   
         }
-
-        // for(vector<int> t: pre){
-        //     for(int a: t){
-        //         cout<<a<<" ";
-        //     }   cout<<endl;
-        // }
 
         for(auto one: mp){
-            ans = max(ans,one.second);
+            ans = max(ans, one.second);
         }
-        //cout<<wall.size();
-        //cout<<" "<<ans;
 
         return wall.size()-ans;
     }
 };
-
-// KPPKKP
-// KKKPKK
-// KPPPKK
-// KKPPPP
-// KKKPKK
-// KPPPKP
