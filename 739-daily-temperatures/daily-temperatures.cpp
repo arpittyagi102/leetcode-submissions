f7151920df6c6861af9637ddf4179083ca69e1ee
Arpit Tyagi <arpittyagi102@gmail.com>
2024-01-31 19:46:04 +0530

    Time: 98 ms (99.24%) | Memory: 100.9 MB (67.32%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> result(temperatures.size(), 0);
+        vector<int> stack;
+
+        for (int i = temperatures.size() - 1; i >= 0; --i) {
+            int currTemperature = temperatures[i];
+
+            while (!stack.empty() && currTemperature >= temperatures[stack.back()]) {
+                stack.pop_back();
+            }
+
+            if (!stack.empty()) {
+                result[i] = stack.back() - i;
+            }
+
+            stack.push_back(i);
+        }
+
+        return result;
+    }
+};
+
