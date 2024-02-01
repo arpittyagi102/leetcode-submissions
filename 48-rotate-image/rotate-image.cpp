96e6b29d29389a05bc8c07fb93c48317e72bdc56
Arpit Tyagi <arpittyagi102@gmail.com>
2024-02-01 22:10:09 +0530

    Time: 4 ms (39.33%) | Memory: 8.6 MB (20.06%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -5,27 +5,13 @@ public:
         int temp;
 
         for(int i=0; i<(n+1)/2; i++){
-            //cout<<"i = "<<i<<endl;
             for(int j=i; j<n-i; j++){
                 temp = matrix[n-j][i];
                 matrix[n-j][i] = matrix[n-i][n-j];
                 matrix[n-i][n-j] = matrix[j][n-i];
                 matrix[j][n-i] = matrix[i][j];
                 matrix[i][j] = temp;
-                // cout<<matrix[i][j]<<" ";
-                // cout<<matrix[j][n-i]<<" ";
-                // cout<<matrix[n-i][n-j]<<" ";
-                // cout<<matrix[n-j][i]<<" ";
-                // cout<<endl;
             }
-            //cout<<endl;
         }
-
-        // for(vector<int> line: matrix){
-        //     for(int one: line){
-        //         cout<<one<<" ";
-        //     }
-        //     cout<<endl;
-        // }
     }
 };
