137b5b93384b2d914e4245635b8a75fea5f8fca8
Arpit Tyagi <arpittyagi102@gmail.com>
2024-03-13 21:10:11 +0530

    Time: 43 ms (7.88%) | Memory: 7.2 MB (41.86%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int s1 = n;
+        int s2 = n * (n + 1) / 2;
+        n--;
+        while(s2 >= s1){
+            if(s2==s1){
+                return n+1;
+            } else {
+                s2 -= n+1;
+                s1 += n--;
+            }
+            cout<<"s1="<<s1<<"  s2="<<s2<<endl;
+        }
+
+        return -1;
+    }
+};
