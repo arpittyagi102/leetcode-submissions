b7bbe6b441e42f4a548d0d714f1dcdb7382f13ca
Arpit Tyagi <arpittyagi102@gmail.com>
2024-05-28 22:35:17 +0530

    Time: 3 ms (37.52%) | Memory: 8.2 MB (74.65%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,33 +1,30 @@
 class MyStack {
 public:
-    queue<int> que1;
-    queue<int> que2;
+    queue<int> que;
 
     MyStack() {}
     
     void push(int x) {
-        que2.push(x);
+        que.push(x);
 
-        while(!que1.empty()){
-            que2.push(que1.front());
-            que1.pop();
+        for(int i=0; i<que.size()-1; i++){
+            que.push(que.front());
+            que.pop();
         }
-
-        swap(que1, que2);
     }
     
     int pop() {
-        int result = que1.front();
-        que1.pop();
+        int popResult = que.front();
+        que.pop();
 
-        return result;
+        return popResult;
     }
     
     int top() {
-        return que1.front();
+        return que.front();
     }
     
     bool empty() {
-        return que1.empty();
+        return que.empty();
     }
 };
