2cf4ef6cc653ff04743503cb7f6dd8c775ca1617
Arpit Tyagi <arpittyagi102@gmail.com>
2024-07-11 00:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (61.11%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
- int step = 0;
-for (const auto& log : logs) {
-    if (log == "../") {
-        if (step > 0) step--;
-    } else if (log != "./") {
-        step++;
-    }
-}
-return step;
+        int step = 0;
+
+        for (string log: logs) {
+            if (log == "../") {
+                if (step > 0) 
+                    step--;
+
+            } else if (log == "./") {
+                // do nothing
+                
+            } else {
+                step++;
+            }
+        }
+        return step;
     }
 };
