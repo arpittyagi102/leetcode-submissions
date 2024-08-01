5529155ead76a02f4841e04ee8e934499905397a
Arpit Tyagi <arpittyagi102@gmail.com>
2024-08-01 21:18:14 +0530

    Time: 6 ms (60.14%), Space: 17.4 MB (31.37%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+
+        for(string detail: details){
+            if(detail[11] > '6' || (detail[11] == '6' && detail[12] != '0')){
+                cout<<detail<<" ";
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
