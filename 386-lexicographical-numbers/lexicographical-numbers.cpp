0e18af692632f772121e1bd46101535127ffa056
Arpit Tyagi <arpittyagi102@gmail.com>
2024-09-21 22:45:29 +0530

    Time: 10 ms (68.77%) | Memory: 13.8 MB (69.49%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
-        vector<string> vec(n);
-        vector<int> ans(n);
-
-        for(int i=0; i<n; i++){
-            vec[i] = to_string(i+1);
+        vector<int> lexicographicalNumbers;
+        // Start generating numbers from 1 to 9
+        for (int start = 1; start <= 9; ++start) {
+            generateLexicalNumbers(start, n, lexicographicalNumbers);
         }
+        return lexicographicalNumbers;
+    }
 
-        sort(vec.begin(), vec.end());
+private:
+    void generateLexicalNumbers(int currentNumber, int limit,
+                                vector<int>& result) {
+        // If the current number exceeds the limit, stop recursion
+        if (currentNumber > limit) return;
 
-        for(int i=0; i<n; i++){
-            ans[i] = stoi(vec[i]);
+        // Add the current number to the result
+        result.push_back(currentNumber);
+
+        // Try to append digits from 0 to 9 to the current number
+        for (int nextDigit = 0; nextDigit <= 9; ++nextDigit) {
+            int nextNumber = currentNumber * 10 + nextDigit;
+            // If the next number is within the limit, continue recursion
+            if (nextNumber <= limit) {
+                generateLexicalNumbers(nextNumber, limit, result);
+            } else {
+                break;  // No need to continue if nextNumber exceeds limit
+            }
         }
-
-        return ans;
     }
 };
