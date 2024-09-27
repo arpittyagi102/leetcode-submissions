8a452519aa34465f3f32db4d4790712bd3d1729d
Arpit Tyagi <arpittyagi102@gmail.com>
2024-09-27 19:42:13 +0530

    Time: 76 ms (69.51%) | Memory: 42.9 MB (36.23%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,23 +1,23 @@
 class MyCalendar {
-    vector<pair<int,int>> bookedTimeSlots;
+private:
+    set<pair<int, int>> calendar;
+
 public:
-    MyCalendar() {
-    }
-    
     bool book(int start, int end) {
-        for(auto slot: bookedTimeSlots){
-            if(slot.first < end && start < slot.second){
+        const pair<int, int> event{start, end};
+        const auto nextEvent = calendar.lower_bound(event);
+        if (nextEvent != calendar.end() && nextEvent->first < end) {
+            return false;
+        }
+
+        if (nextEvent != calendar.begin()) {
+            const auto prevEvent = prev(nextEvent);
+            if (prevEvent->second > start) {
                 return false;
             }
         }
 
-        bookedTimeSlots.push_back(make_pair(start,end));
+        calendar.insert(event);
         return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
