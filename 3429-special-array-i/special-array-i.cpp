828e5088f0d9cdb7df321938bb2ea5397388fa1d
Arpit Tyagi <arpittyagi102@gmail.com>
2024-12-09 10:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (6.87%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        if(nums.size()<2){
-            return true;
-        }
-
-        bool type = nums[0]%2 == 0;
-
-        for(int i=0; i<nums.size(); i++){
-            if(i%2 == 0 && type != (nums[i]%2==0)){
-                return false;
-            }
-
-            if(i%2 != 0 && type == (nums[i]%2==0)){
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]%2 == nums[i+1]%2){
                 return false;
             }
         }
