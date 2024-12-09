88ed84d5481d8ed43a6443bb945e27d5dc12331d
Arpit Tyagi <arpittyagi102@gmail.com>
2024-12-09 10:40:04 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (6.87%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -7,14 +7,12 @@ public:
 
         bool type = nums[0]%2 == 0;
 
-        for(int i=0; i<nums.size(); i=i+2){
-            if(type != (nums[i]%2==0)){
+        for(int i=0; i<nums.size(); i++){
+            if(i%2 == 0 && type != (nums[i]%2==0)){
                 return false;
             }
-        }
 
-        for(int i=1; i<nums.size(); i=i+2){
-            if(type == (nums[i]%2==0)){
+            if(i%2 != 0 && type == (nums[i]%2==0)){
                 return false;
             }
         }
