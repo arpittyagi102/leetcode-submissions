056cd01a9877b285ed8aca9b38ea7333a1e41a91
Arpit Tyagi <arpittyagi102@gmail.com>
2025-04-08 10:43:09 +0530

    Time: 1 ms (65.36%), Space: 27.8 MB (54.27%) - LeetHub

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        unordered_set<int> st;
+        int rem = nums.size() % 3;
+
+        // checking for last few(0,1,2) numbers
+        for(int i=nums.size()-1; i>=(int)(nums.size()-rem); i--){
+            cout<<nums[i]<<" ";
+            if(st.find(nums[i]) != st.end()){
+                return nums.size()/3 + 1;
+            }
+            st.insert(nums[i]);
+        }  
+
+        // checking for remaining numbers (multiple of 3)
+        for(int i=nums.size()-1-rem; i>=0; i--){
+            if(st.find(nums[i]) != st.end()){
+                return ceil((double)(i+1)/3);
+            }
+            st.insert(nums[i]);
+        }         
+
+        return 0;
+    }
+};
