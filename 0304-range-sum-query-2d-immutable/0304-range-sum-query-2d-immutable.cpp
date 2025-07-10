7ac795c9a7701637a7dbf6c266f10f4b5ff95dda
Arpit Tyagi <arpittyagi102@gmail.com>
2025-07-10 21:22:34 +0530

    Time: 17 ms (83.01%), Space: 149.1 MB (84.88%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -5,26 +5,26 @@ public:
     NumMatrix(vector<vector<int>>& matrix) {
         prefixSum = vector<vector<int>>(matrix.size(), vector<int>(matrix[0].size()));
 
-        for(int i=0; i<matrix.size(); i++) {
-            prefixSum[i][0] = matrix[i][0];
+        prefixSum[0][0] = matrix[0][0];
+        for(int j=1; j<matrix[0].size(); j++) {
+            prefixSum[0][j] = matrix[0][j] + prefixSum[0][j-1];
+        }
+
+        for(int i=1; i<matrix.size(); i++) {
+            prefixSum[i][0] = matrix[i][0] + prefixSum[i-1][0];
             for(int j=1; j<matrix[i].size(); j++) {
-                prefixSum[i][j] = matrix[i][j] + prefixSum[i][j-1];
+                prefixSum[i][j] = matrix[i][j] + prefixSum[i][j-1] + prefixSum[i-1][j] - prefixSum[i-1][j-1];
             }
         }
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum = 0;
+        int ii = (row1 > 0 && col1 > 0) ? prefixSum[row1-1][col1-1] : 0;
+        int ij = (row1 > 0) ? prefixSum[row1-1][col2] : 0;
+        int ji = (col1 > 0) ? prefixSum[row2][col1-1] : 0;
+        int jj = prefixSum[row2][col2];
 
-        for(int i=row1; i<=row2; i++){
-            if (col1 == 0){
-                sum += prefixSum[i][col2];
-            } else {
-                sum += prefixSum[i][col2] - prefixSum[i][col1 - 1];
-            }
-        }
-
-        return sum;
+        return jj - ij - ji + ii;
     }
 };
 
