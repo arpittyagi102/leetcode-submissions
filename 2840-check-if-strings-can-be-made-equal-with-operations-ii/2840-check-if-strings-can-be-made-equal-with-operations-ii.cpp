3e9ebfac02a8960dae4e14589d1d221602f90a5a
Arpit Tyagi <arpittyagi102@gmail.com>
2026-03-30 11:47:18 +0530

    Time: 4 ms (83.33%), Space: 19.5 MB (84.48%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -1,29 +1,18 @@
 class Solution {
 public:
     bool checkStrings(string s1, string s2) {
-        unordered_map<char, int> mapEven;
-        unordered_map<char, int> mapOdd;
+        vector<int> mp(52);
 
-        for(int i=0; i<s1.size(); i++)
-            if(i%2)
-                mapOdd[s1[i]]++;
-            else
-                mapEven[s1[i]]++;
+        for(int i=0; i<s1.size(); i++) {
+            int offset = (i & 1) * 26;
+            mp[s1[i] - 'a' + offset]++;
+            mp[s2[i] - 'a' + offset]--;
+        }
 
-        for(int i=0; i<s2.size(); i++)
-            if(i%2)
-                mapOdd[s2[i]]--;
-            else
-                mapEven[s2[i]]--;
-
-        for(auto pair: mapOdd)
-            if(pair.second)
-                return false;
-        
-        for(auto pair: mapEven)
-            if(pair.second)
+        for(int count: mp)
+            if(count)
                 return false;
 
-        return true;       
+        return true;    
     }
 };
