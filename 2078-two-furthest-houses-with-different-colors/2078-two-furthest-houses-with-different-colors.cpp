ca038b46c406481e8ee8dcb3d83b31e4bcb628b1
Arpit Tyagi <arpittyagi102@gmail.com>
2026-04-20 11:34:25 +0530

    Time: 0 ms (100%), Space: 12.1 MB (40.24%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -4,22 +4,11 @@ public:
         int first = colors[0];
         int last = colors[colors.size()-1];
         int n = colors.size();
-        int ans = 0;
 
-        for(int i=0; i<n; i++) {
-            if(last != colors[i]) {
-                ans = max(ans, n-i-1);
-                break;
-            }
-        }
+        for(int i=0; i<n; i++)
+            if(last != colors[i] || first != colors[n-i-1])
+                return n-i-1;
 
-        for(int i=n-1; i>=0; i--) {
-            if(first != colors[i]) {
-                ans = max(ans, i);
-                break;
-            }
-        }
-
-        return ans;
+        return 0;
     }
 };
