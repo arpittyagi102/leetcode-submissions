03653c91ea8f31b3bbb211184cc6d36f2253c5b1
Arpit Tyagi <arpittyagi102@gmail.com>
2026-05-14 13:01:19 +0530

    Time: 0 ms (100%), Space: 27.2 MB (31.27%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     bool isGood(vector<int>& nums) {
         int n = nums.size();
+        int lastNumFreq = 0;
         vector<int> freq(n,0);
 
-        for(int num: nums){
-            if(num < 1 || num >= n)
+        for(int i=0; i<nums.size(); i++){
+            int value = abs(nums[i]);
+
+            if(value < 1 || value >= n)
+                return false;
+            
+            if(value != n-1 && nums[value-1] < 0)
+                return false;
+            if(value == n-1 && lastNumFreq > 1)
                 return false;
 
-            if(num != n-1 && freq[num]>0)
-                return false;
-            else if(num == n-1 && freq[num]>1)
-                return false;
-
-            freq[num]++;
+            nums[value-1] *= -1;
+            if(value == n-1)
+                lastNumFreq++;
         }            
 
-        return true;
+        return lastNumFreq == 2;
     }
 };
