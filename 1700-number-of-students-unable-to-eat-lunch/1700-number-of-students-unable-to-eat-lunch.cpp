12954e64977d3901685d13358524e6eee70a74cb
Arpit Tyagi <arpittyagi102@gmail.com>
2026-07-24 11:51:12 +0530

    Time: 0 ms (100%), Space: 11.7 MB (91.06%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -2,34 +2,17 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         stack<int> sk;
-        queue<int> q;
+        int q[2] = {0, 0};
 
         for(int student : students)
-            q.push(student);
-
-        for(int i=sandwiches.size()-1; i>=0; i--)
-            sk.push(sandwiches[i]);
-
-        int size = q.size();
+            q[student]++;
         
-        while(!sk.empty()) {
-            if(sk.top() == q.front()) {
-                sk.pop();
-                q.pop();
-                size = q.size();
-            }
-            else {
-                if(size == 0) {
-                    return q.size();
-                }
+        for (int s : sandwiches)
+            if (q[s] == 0)
+                break;
+            else 
+                q[s]--;
 
-                int front = q.front();
-                q.pop();
-                q.push(front);
-                size--;
-            }
-        }
-
-        return q.size();
+        return q[0] + q[1];
     }
 };
