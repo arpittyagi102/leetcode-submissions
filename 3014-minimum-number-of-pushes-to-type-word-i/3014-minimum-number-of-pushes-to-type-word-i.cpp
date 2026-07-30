5372cea6d8cd64b69475a96ef01b6df876032f8e
Arpit Tyagi <arpittyagi102@gmail.com>
2026-07-30 11:04:09 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,7 +1,6 @@
+int LUT[27] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  10, 12, 14, 16, 18,
+               20, 22, 24, 27, 30, 33, 36, 39, 42, 45, 48, 52, 56};
 class Solution {
 public:
-    int minimumPushes(string word) {
-        int n = word.size() / 8;
-        return (n * (n + 1) * 4) + ((n + 1) * (word.size() % 8));
-    }
+    int minimumPushes(string word) { return LUT[word.size()]; }
 };
